f5744379a3db0f91ee6128fdff513e7cde5798ee
criptodev <cesarmaximo91@gmail.com>
2017-06-03 18:28:10 -0400

    next

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-//Given a number search in on array, and if is onordered(the array), order the array first.
+/*
+/Given a number search in on array, and if is onordered(the array), order the array first.
 //array example:
-var myArray = [4, 10, 15, 123, 19, 14, -15]//, 0, 61, 91, 157, 1, 8, 0.5];
+var myArray = [4, 10, 15, 123, 19, 14, 16]//, 0, 61, 91, 157, 1, 8, 0.5];
 //formule:
 var lastNum = myArray.length - 1;
-var father = Math.floor((lastNum - 1) / 2);
+
 //The cicle start whith the array length and every cicle the array need to be reduced by 1, finish when the array have only one nomber
-for (var j = lastNum; j < myArray.length; j++) {
+for (var j = lastNum; j > 0; j--) {
+  //father formule:
+  var father = Math.floor((j - 1) / 2);
   //cicle to find the higher number
   for(var i = father; i >= 0; i--){
     //formule children
@@ -14,18 +17,48 @@ for (var j = lastNum; j < myArray.length; j++) {
     //swap numbers to put the higher at first position in the array
     if (myArray[i] < myArray[leftCh]) {
       var tempFather = myArray[i]
-      myArray[i] = myArray[leftCh];
-      myArray[leftCh] = tempFather
+          myArray[i] = myArray[leftCh];
+          myArray[leftCh] = tempFather
+
     }
     if (myArray[i] < myArray[rightCh]) {
       var tempFather2 = myArray[i]
-      myArray[i] = myArray[rightCh];
-      myArray[rightCh] = tempFather
+          myArray[i] = myArray[rightCh];
+          myArray[rightCh] = tempFather
     }
     //swap the first number of the array with the last number
     var tempHigh = myArray[0];
-    myArray[0] = myArray[lastNum];
-    myArray[lastNum] = tempHigh;
+        myArray[0] = myArray[j];
+        myArray[j] = tempHigh;
   }
 }
-console.log(myArray);
+*/
+//Given a number search in on array, and if is onordered(the array), order the array first.
+//array example:
+var myArray = [4, 10, 15, 91, 61, 19, 157];//, 8, 0.5];
+//formule:
+var lastNum = myArray.length - 1;
+for (var j = lastNum; j > 0; j--) {
+  console.log(j);
+  //cicle to find the higher number
+  for(var i = Math.floor((j - 1) / 2); i >= 0; i--){
+    //formule children
+    var leftCh = 2 * i + 1;
+    var rightCh = 2 * i + 2;
+    //swap numbers to put the higher at first position in the array
+    if (myArray[i] < myArray[leftCh]) {
+      var tempFather = myArray[i]
+          myArray[i] = myArray[leftCh];
+          myArray[leftCh] = tempFather;
+    }
+    if (myArray[i] < myArray[rightCh]) {
+      var tempFather2 = myArray[i]
+          myArray[i] = myArray[rightCh];
+          myArray[rightCh] = tempFather2;
+    }
+  }
+  //swap the first number of the array with the last number
+  var tempHigh = myArray[0];
+      myArray[0] = myArray[j];
+      myArray[j] = tempHigh;
+}
